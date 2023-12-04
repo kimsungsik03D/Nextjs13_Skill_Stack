490dcbaafac11a8d576dec925a8224d25b73a75a
sungsik:D <tjdtlr2620@gmail.com>
2023-12-04 16:37:45 +0900

    UPDATE block link

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <Link href="/board">1.Dashboard</Link>
+      {/* <Link href="/board">1.Dashboard</Link> */}
       <br />
       <Link href="/htmlcss">2.htmlcss</Link>
     </main>
